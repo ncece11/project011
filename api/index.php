2392423e9f3ff9d2b242351502e9d8d1b3d5887c
ncece11 <75845052+ncece11@users.noreply.github.com>
2023-05-15 15:57:48 +0800

    edit

--- api/index.php
+++ api/index.php
@@ -1,17 +1,3 @@
 <?php
-phpinfo();
+    phpinfo();
 ?>
-
-
-<h1><span><</span>05<span>></span></h1><hr>
-<h1><span><</span>05<span>></span></h1><hr>
-<h1><span><</span>05<span>></span></h1><hr>
-<h1><span><</span>05<span>></span></h1><hr>
-<h1><span><</span>05<span>></span></h1><hr>
-
-<h1><span><</span>05<span>></span></h1><hr>
-<h1><span><</span>05<span>></span></h1><hr>
-<h1><span><</span>05<span>></span></h1><hr>
-<h1><span><</span>05<span>></span></h1><hr>
-<h1><span><</span>05<span>></span></h1><hr>
-
